770fccf1e507b9066bfe0a257fd045b0df05f092
Will Usher <willusher.life@gmail.com>
2015-09-15 13:18:16 -0600

    test cl now runs on Python 3.5
    Also added per-case grading so we won't count multiple issues in one
    case as multiple failures but just a failing of that test case.
    
    The warnings and error checker will use regex to find matches and will
    also log the warnings and errors we got to the result text file so the
    student can see this output when we grade them.

--- test_cl.py
+++ test_cl.py
@@ -3,9 +3,10 @@ import subprocess
 import sys
 import difflib
 import re
+import shutil
 
 if len(sys.argv) < 3:
-    print 'Usage: test_cl.py homework compile/run/check/grade'
+    print('Usage: test_cl.py homework compile/run/check/grade')
     sys.exit()
 
 notepad_file = 'C:/Program Files (x86)/Notepad++/notepad++.exe'
@@ -14,63 +15,98 @@ editor = notepad_file
 if sys.argv[2] == 'grade':
     if not os.path.isfile(editor):
         if editor == notepad_file:
-            print 'Please install Notepad++'
+            print('Please install Notepad++')
         else:
-            print 'Please install gvim'
+            print('Please install gvim')
         sys.exit()
 
 # compare the output of a student's program to the reference output, writing the results into a
 # given result file
 def compare(reference_output, student_output, result_file):
     diff = ''
-    difference_count = 0
+    case_failed_count = 0
     case_number = 0
+    case_failed = False
     match_case_number = re.compile(" Case (\d+):")
     with open(reference_output, 'r') as ref_out:
         with open(student_output, 'r') as student_out:
             reference = ref_out.readlines()
             student = student_out.readlines()
             # TODO: Strip leading/trailing whitespace and newlines from student solution
-            # TODO: Track which test case we're in and if we've already deducted to not
-            # deduct multiple times for one issue
+            # but don't strip the last newline character? Or strip all trailing and stick
+            # the last newline character back on
             for line in difflib.unified_diff(reference, student, fromfile='reference', tofile='student'):
-                print line
+                print(line)
                 case_match = match_case_number.match(line)
                 if case_match:
                     case_number = int(case_match.group(1))
-                    print 'Now in case ', case_number
-                if line.startswith('-'):
-                    difference_count += 1
+                    case_failed = False
+                if not case_failed and line.startswith('-') and not line.startswith('---'):
+                    case_failed_count += 1
+                    case_failed = True
                 diff += line
     with open(result_file, 'w') as result_out:
         result_out.write(diff)
-        result_out.write('\nDifference Count: ' + str(difference_count) + '\n')
+        result_out.write('\nCases Failed: ' + str(case_failed_count) + '\n')
+        result_out.write('Total Cases: ' + str(case_number + 1) + '\n')
 
+match_warning = re.compile('.*: warning C\d+:')
+match_error = re.compile('.*: error C\d+:')
 # count the number of compiler warnings and errors in the input file, and write the results to the
 # output file
 def count_warnings_errors(input_file, output_file):
-    warning_count = 0
-    error_count = 0
+    warnings = []
+    errors = []
     with open(input_file, 'r') as f:
         content = f.readlines()
         for line in content:
-            if line.find('warning C') != -1:
-                warning_count += 1
-            elif line.find('error C') != -1:
-                error_count += 1
+            print(line)
+            warning = match_warning.match(line)
+            if warning:
+                warnings.append(line)
+            error = match_error.match(line)
+            if error:
+                errors.append(line)
     with open(output_file, 'a') as f:
         f.write('\n')
-        f.write('Warnings: ' + str(warning_count) + '\n')
-        f.write('Errors: ' + str(error_count) + '\n')
+        f.write('Warnings: ' + str(len(warnings)) + '\n')
+        f.write(''.join(warnings) + '\n')
+        f.write('Errors: ' + str(len(errors)) + '\n')
+        f.write(''.join(errors))
 
-print 'Grading', sys.argv[1]
+# Open files for final grading
+def grade(file, stdout_file, result_file, grade_file, ref_stdout_file):
+    # Copy autograde summary (diff, warnings, failed case report) to the
+    # final grade file
+    shutil.copyfile(result_file, grade_file)
+    if os.path.isfile(ref_stdout_file):
+        subprocess.call([editor, file, result_file, stdout_file, grade_file])
+    else:
+        subprocess.call([editor, file, result_file, stdout_file, ref_stdout_file,
+            grade_file])
+
+match_score = re.compile("Grade: (\d+)")
+# Check that a score was correctly assigned to the problem
+def check_grading(grade_file):
+    grade_content = open(grade_file, 'r').readlines()
+    return not (match_score.match(grade_content[-1]) is None)
+
+print('Grading ' + sys.argv[1])
 main_dir = os.path.abspath('.')
 homework_dir = os.path.abspath('./submissions/' + sys.argv[1])
-for student_dir in os.walk(homework_dir).next()[1]:
+# Collect the list of all student directories
+student_dirs = []
+for _, s, _ in os.walk(homework_dir):
+    student_dirs += s
+
+for student_dir in student_dirs:
     student_dir = os.path.abspath(homework_dir + '/' + student_dir)
     os.chdir(student_dir)
-    print student_dir
-    for file in os.walk(student_dir).next()[2]:
+    # Collect the list of all of the student's files
+    files = []
+    for _, _, f in os.walk(student_dir):
+        files += f
+    for file in files:
         base, ext = os.path.splitext(file)
         if ext == '.cpp' or ext == '.cc':
             cl_stdout_file = base + '_cl.txt'
@@ -80,31 +116,38 @@ for student_dir in os.walk(homework_dir).next()[1]:
             result_file = base + '_results.txt'
             grade_file = base + '_grade.txt'
             check_prog = homework_dir + '/' + base + '_check.exe'
-            if (sys.argv[2] == 'compile'): # compile
-                print 'Compiling ' + file
+            # Compile student's programs
+            if sys.argv[2] == 'compile':
+                print('Compiling ' + file)
                 with open(cl_stdout_file, 'w') as cl_stdout:
-                    subprocess.Popen(['cl.exe', '/W4', '/EHsc', file], stdout=cl_stdout, universal_newlines=True)
-            elif (sys.argv[2] == 'run'): # run
-                print 'Running ' + base + '.exe'
-                if (os.path.isfile(stdin_file)): # run with input
+                    subprocess.Popen(['cl.exe', '/W4', '/EHsc', file], stdout=cl_stdout,
+                            universal_newlines=True)
+            # Run all student programs and save output results
+            elif sys.argv[2] == 'run':
+                print('Running ' + base + '.exe')
+                if os.path.isfile(stdin_file): # run with input
                     with open(stdin_file, 'r') as stdin_, open(stdout_file, 'w') as stdout_:
                         subprocess.Popen([base], stdin=stdin_, stdout=stdout_, universal_newlines=True)
                 else: # run without input
                     if (os.path.isfile('./' + base + '.exe')):
                         with open(stdout_file, 'w') as stdout_:
                             subprocess.Popen([base + '.exe'], stdout=stdout_, universal_newlines=True)
-            elif (sys.argv[2] == 'check'): # check outputs
-                print 'Checking ' + base
+            # Diff student outputs with the expected solution
+            elif sys.argv[2] == 'check':
+                print('Checking ' + base)
                 if (os.path.isfile(check_prog)): # use the check program
                     with open(stdout_file, 'r') as stdout_, open(result_file, 'w') as result_:
-                        subprocess.Popen([check_prog], stdin=stdout_, stdout=result_, universal_newlines=True)
+                        subprocess.Popen([check_prog], stdin=stdout_, stdout=result_,
+                                universal_newlines=True)
                 else: # simply compare output files
                     compare(ref_stdout_file, stdout_file, result_file)
                 # count the number of warnings and errors
                 count_warnings_errors(cl_stdout_file, result_file)
-            elif (sys.argv[2] == 'grade'): # open the relevant text files to grade
-                open(grade_file, 'a').close()
-                if os.path.isfile(ref_stdout_file):
-                    subprocess.call([notepad_file, file, result_file, stdout_file, grade_file])
-                else:
-                    subprocess.call([notepad_file, file, result_file, stdout_file, ref_stdout_file, grade_file])
+            # Open the student programs and outputs for final grading
+            elif sys.argv[2] == 'grade':
+                grade(file, stdout_file, result_file, grade_file, ref_stdout_file)
+                # Check that a final grade for the assignment has been entered in the grade file
+                while not check_grading(grade_file):
+                    print("Error! No grade assigned for " + file)
+                    grade(file, stdout_file, result_file, grade_file, ref_stdout_file)
+
